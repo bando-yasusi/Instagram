304b39e704a97faa09068622b8fec5577c89652a
yasusi.bando <bando057@outlook.jp>
2025-06-23 12:51:58 +0900

    p129途中

---
+++ PostTableViewCell.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var postImageView: UIImageView!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var likeLabel: UILabel!
+    @IBOutlet weak var dateLabe: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
